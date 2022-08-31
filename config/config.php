46bb75d016698c145c8e12a9b20c90fef5e76c21
Igor360 <igor.dawidencko@gmail.com>
2022-08-31 10:38:04 +0300

    Fix configs namespaces

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    "erc20ABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadERC20ABI(),
-    "erc721ABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadERC721ABI(),
-    "erc1155ABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadERC721ABI(),
-    "wethABI" =>  \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadWETHABI(),
+    "erc20ABI" => \Igor360\NftEthPhpConnector\Configs\ConfigFacade::loadERC20ABI(),
+    "erc721ABI" => \Igor360\NftEthPhpConnector\Configs\ConfigFacade::loadERC721ABI(),
+    "erc1155ABI" => \Igor360\NftEthPhpConnector\Configs\ConfigFacade::loadERC1155V2ABI(),
+    "wethABI" =>  \Igor360\NftEthPhpConnector\Configs\ConfigFacade::loadWETHABI(),
     "eth" => [
         "host" => "",
         "port" => "",
